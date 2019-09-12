62a2c0012f2d69e73ba9199cf5c9b3db798ad526
aa-ahmed-aa <ahmedkhaled36@hotmail.com>
2019-09-12 12:57:23 +0200

    insert the user

--- src/Component/User/Manager/UserManager.php
+++ src/Component/User/Manager/UserManager.php
@@ -5,14 +5,17 @@ namespace App\Component\User\Manager;
 use App\Component\User\Model\User;
 use App\Component\Manager\BaseManager;
 use App\Component\User\Repository\UserRepository;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class UserManager extends BaseManager
 {
     private $userRepository;
+    private $encoder;
 
-    public function __construct(UserRepository $userRepository)
+    public function __construct(UserRepository $userRepository, UserPasswordEncoderInterface $encoder)
     {
         $this->userRepository = $userRepository;
+        $this->encoder = $encoder;
     }
 
     public function getSubscriber($bot)
@@ -20,21 +23,28 @@ class UserManager extends BaseManager
         $user = $this->userRepository->findOneBy(['fasebookid' => $bot->getUser()->getId()]);
 
         if (empty($user)) {
-            /**
-             * Will register facebook user with id
-             */
-            $entityManager = $this->getDoctrine()->getManager();
-
-            $user = new User();
-            $user->setUsername($bot->getUser()->getFirstName());
-            $user->setPassword($this->encoder->encodePassword($user, 'ahmedkhaled'));
-            $user->setEmail($bot->getUser()->getFirstName().'@'.$bot->getUser()->getLastName().'.com');
-            $user->setFasebookid($bot->getUser()->getId());
-
-            $entityManager->persist($user);
-            $entityManager->flush();
-
+                
+                $user = $this->insertUser($bot);
         }
+
+        return $user;
+    }
+
+    /**
+        * Will register facebook user with id
+        */
+    public function insertUser($bot)
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $user = new User();
+        $user->setUsername($bot->getUser()->getFirstName());
+        $user->setPassword($this->encoder->encodePassword($user, 'ahmedkhaled'));
+        $user->setEmail($bot->getUser()->getFirstName().'@'.$bot->getUser()->getLastName().'.com');
+        $user->setFasebookid($bot->getUser()->getId());
+
+        $entityManager->persist($user);
+        $entityManager->flush();
 
         return $user;
     }
